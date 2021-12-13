583173d8577f870f307af123c8ea2185292afbdf
Mayur Botre <mayurbotre95@gmail.com>
2021-12-13 22:53:56 +0530

    Final Project Commit

--- app/src/main/java/com/example/watersprinkler/HomeActivity.java
+++ app/src/main/java/com/example/watersprinkler/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.watersprinkler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,38 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void sprinklerData1(View view) {
+        Intent i = new Intent(getApplicationContext(), TempHumActivity.class);
+        startActivity(i);
+    }
+    public void sprinklerData2(View view) {
+        Intent i = new Intent(getApplicationContext(), TempHumActivity.class);
+        startActivity(i);
+    }
+    public void sprinklerData3(View view) {
+        Intent i = new Intent(getApplicationContext(), TempHumActivity.class);
+        startActivity(i);
+    }
+    public void sprinklerData4(View view) {
+        Intent i = new Intent(getApplicationContext(), TempHumActivity.class);
+        startActivity(i);
+    }
+    public void sprinklerData5(View view) {
+        Intent i = new Intent(getApplicationContext(), TempHumActivity.class);
+        startActivity(i);
+    }
+    public void sprinklerData6(View view) {
+        Intent i = new Intent(getApplicationContext(), TempHumActivity.class);
+        startActivity(i);
+    }
+    public void sprinklerData7(View view) {
+        Intent i = new Intent(getApplicationContext(), TempHumActivity.class);
+        startActivity(i);
+    }
+    public void sprinklerData8(View view) {
+        Intent i = new Intent(getApplicationContext(), TempHumActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/watersprinkler/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.example.watersprinkler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,HomeActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                finish();
+            }
+        },400);
+    }
+}
